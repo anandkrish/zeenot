e93372f11fcd63fcf326a2dcb73168260fba604e
xzion <anand.121991@gmail.com>
2011-10-27 11:06:15 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,51 +1,60 @@
 #!/usr/bin/python
-banddict={0:'Black',1:'Brown',2:'Red',3:'Orange',4:'Yellow',5:'Green',6:'Blue',7:'Violet',8:'Gray',9:'White'}
-global q,r,n,band1,band2,band3,man
-print 'Enter the value of resistor '
-man=input('Enter th value of mantissa : ')
-exp=raw_input('Enter th value of exponent : ')
-intflag=isinstance(man,int)
-print intflag
-if intflag==1:
-	print intflag
-	if man>0 and man<10:
-		q=man
-		r=0
-		n=0
-	elif man>=10 and man<100:
-		q=man/10
-		r=man%10
-		n=0
-	elif man>=100 and man<1000:
-		q=man/100
-		r=((man/10)%10)
-		n=1
-	else:
-		print 'Error'
-else:
-	if man>0 and man<10:
-		e=man*10
-		f=e/10
-		q=int(f)
-		g=e%10
-		r=int(g)
-		n=-1
-	else:
-		print "error"
-if exp=='k':
-	band1=q
-	band2=r
-	band3=3+n
-elif exp=='m':
-	band1=q
-	band2=r
-	band3=6+n
-else:
-	band1=q
-	band2=r
-	band3=n
+r=input('enter the resistor value')
+u=input('enter the unit:1 for ohm,2 for kiloohm,3 for megaohm')
+def color_code(t,o,p):
+			d={0:'black',1:'brown',2:'red',3:'orange',4:'yellow',5:'green',6:'blue',7:'violet',8:'gray',9:'white',10:'no_band'}
+			print 'band1=',d[t]
+			print 'band2=',d[o]
+			print 'band3=',d[p]
 
-print 'Band 1 =',banddict[band1]
-print 'Band 2 =',banddict[band2]
-print 'Band 3 =',banddict[band3]
-
+if u==1:
+		if r<10:
+			flag=isinstance(r,int)
+			if flag==1:
+					color_code(10,r,10)
+			else:
+				print "invalid input"
+		elif r > 9 and r <100:
+	  				color_code(r/10,r%10,10)
+		elif r > 99 and r < 1000:
+						if r%10==0:
+								color_code(r/100,((r/10)%10),1)
+						else:	
+							print "invalid input"
+		
+elif u==2:
+		e=3			
+		if r<10:
+			flag=isinstance(r,int)
+			if flag==1:
+					color_code(r,0,e-1)
+			else:
+				r1=r*10
+				color_code(int(r1)/10,int(r1)%10,e-1)
+		elif r in range(10,100):
+					color_code(r/10,r%10,e)
+		elif r>99  and r<1000:	
+					if r%10==0:
+							color_code(r/100,((r/10)%10),e+1)
+					else:
+						print "invalid input"
+					
+else:
+		e=6
+		if r<10:
+			flag=isinstance(r,int)
+			if flag==1:
+					color_code(r,0,e-1)
+			else:
+					r1=r*10
+					color_code(int(r1)/10,int(r1)%10,e-1)
+							
+		elif r in range(10,100):
+					color_code(r/10,r%10,e)
+		elif r>99  and r<1000:	
+					if r%10==0:
+							color_code(r/100,((r/10)%10),e+1)
+					else:
+						print "invalid input"
+		
+							
